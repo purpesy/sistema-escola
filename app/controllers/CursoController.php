0e2a8c2e75cc3c36fb7f11d0a44ce7382a30e601
purpesy <lucasgabrieldossantos753@gmail.com>
2025-05-15 14:56:46 -0300

    Implementar formulário de criação de curso com validação e tratamento de imagem

--- app/controllers/CursoController.php
+++ app/controllers/CursoController.php
@@ -88,6 +88,47 @@ class CursoController extends Controller
     public function criar()
     {
         $dados = array();
+        // 1 chamada do botao novo curso.
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            // 2 pegar os dados do form
+            $nome_curso = filter_input(INPUT_POST, 'nome_curso', FILTER_SANITIZE_SPECIAL_CHARS);
+            $descricao_curso = filter_input(INPUT_POST, 'descricao_curso', FILTER_SANITIZE_SPECIAL_CHARS);
+            $pre_requisito_curso = filter_input(INPUT_POST, 'pre_requisito_curso', FILTER_SANITIZE_SPECIAL_CHARS);
+            $valor_curso = filter_input(INPUT_POST, 'valor_curso', FILTER_SANITIZE_NUMBER_FLOAT);
+            $carga_horaria_curso = filter_input(INPUT_POST, 'carga_horaria_curso', FILTER_SANITIZE_NUMBER_INT);
+            $area_curso = filter_input(INPUT_POST, 'area_curso', FILTER_SANITIZE_SPECIAL_CHARS);
+            $nivel_curso = filter_input(INPUT_POST, 'nivel_curso', FILTER_SANITIZE_SPECIAL_CHARS);
+            $modalidade_curso = filter_input(INPUT_POST, 'modalidade_curso', FILTER_SANITIZE_SPECIAL_CHARS);
+            $alt_curso = $nome_curso;
+            date_default_timezone_get('America/Sao_Paulo');
+            $data_criacao_curso = date('y-m-d H:i:s');
+            $data_atualizacao_curso = date('y-m-d H:i:s');
+            $status_curso = 'Pendente';
+        }
+        // 3 Inserir os dados no banco]
+        if ($nome_curso && $nivel_curso && $carga_horaria_curso) {
+            $dadosCurso = array(
+                'nome_curso' => $nome_curso,
+                'descricao_curso' => $descricao_curso,
+                'pre_requisito_curso' => $pre_requisito_curso,
+                'valor_curso' => $valor_curso,
+                'carga_horaria_curso' => $carga_horaria_curso,
+                'area_curso' => $area_curso,
+                'nivel_curso' => $nivel_curso,
+                'modalidade_curso' => $modalidade_curso,
+                'alt_curso' => $alt_curso,
+                'data_criacao_curso' => $data_criacao_curso,
+                'data_atualizacao_curso' => $data_atualizacao_curso,
+                'status_curso' => $status_curso
+            );
+        }
+
+        // 4 Tratar o nome da imagem e salvar na pasta uploads
+
+        // 5 Atualizar o registro do curso, com a foto
+
+        // 6 fazer um alerta de sucesso ou erro na pagina de listar os cursos
 
         $dados['conteudo'] = 'admin/curso/criar';
         $this->carregarViews('admin/dash', $dados);
